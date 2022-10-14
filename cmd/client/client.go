0d849406266da258069d9fe5e4a3482e920a0225
marco <marco@joinder.me>
2022-10-13 22:19:49 -0300

    Developing stream beetwen cliente and server - Server sending multiple responses

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"testing-grpc-with-go/pb"
 
@@ -19,24 +20,58 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	//AddUser(client)
+	AddUserVerbose(client)
 
 }
 
-func AddUser(client pb.UserServiceClient){
+func AddUser(client pb.UserServiceClient) {
 
 	req := &pb.User{
-		Id: "0",
-		Name: "Joao",
+		Id:    "0",
+		Name:  "Joao",
 		Email: "j@j.com",
 	}
 
 	res, err := client.AddUser(context.Background(), req)
-	
+
 	if err != nil {
 		log.Fatalf("Could not make connect to gRPC request: %v", err)
 	}
 
 	fmt.Println(res)
+
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Joao",
+		Email: "j@j.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Could not make connect to gRPC request: %v", err)
+	}
+
+	for {
+
+		stream, err := responseStream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive the msg: %v", err)
+
+		}
+
+		fmt.Println("Status: ", stream.Status, " - ", stream.GetUser())
+
+	}
 
 }
--- services/user.go
+++ services/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing-grpc-with-go/pb"
+	"time"
 )
 
 //type UserServiceServer interface {
@@ -29,5 +30,48 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name: req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer)  error{
+
+	stream.Send(&pb.UserResultStream{
+		Status: "init",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+		Id: "123",
+		Name: req.GetName(),
+		Email: req.GetEmail(),
+		}, 
+	})
+
+	time.Sleep(time.Second * 3)
+
+	
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+		Id: "123",
+		Name: req.GetName(),
+		Email: req.GetEmail(),
+		}, 
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 
 }
